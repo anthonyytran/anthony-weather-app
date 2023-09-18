1a68d61d87ae91d6a299d9ef0a4746b72df74852
DESKTOP-LGO4SDH\Anthony <s3780694@student.rmit.edu.au>
2023-09-19 01:05:37 +1000

    Autocomplete feature complete

---
+++ src/api/api.js
@@ -0,0 +1,8 @@
+export const geoApiOptions = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": 'aec2fe8fb7mshd2347c762e24018p1f14ebjsnf0e5bc13fd6d',
+      "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    },
+  };
+  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
