bd80fafd5cfc05a61d276fc9e3c75ed02d0dc5d3
DESKTOP-LGO4SDH\Anthony <s3780694@student.rmit.edu.au>
2023-09-19 18:08:53 +1000

    added icon

--- src/components/current-weather/current-weather.js
+++ src/components/current-weather/current-weather.js
@@ -1,7 +1,15 @@
 import "./current-weather.css"
 
 const CurrentWeather = () => {
-    return "Hello";
+    return (
+        <div className="weather">
+            <div clasName="top">
+                <p className="city">Belgrade</p>
+                <p className="weather-description">Sunny</p>
+            </div>
+            <img alt="weather" className="weather-icon" src="icons/01d.png" />
+        </div>
+    );
 }
 
 export default CurrentWeather
